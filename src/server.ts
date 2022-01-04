c060e3ab3ddeaea9e6ec24bd14d144fd5188db0d
Marcus Santos <marcus.santos@uppercase.com.br>
2022-01-04 13:43:59 -0300

    feat: :sparkles: adicionado dockerfile e docker-compose para adicionar container a aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
+import './database';
 
 const app = express();
 app.use(express.json());
@@ -10,4 +11,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () =>{
+  console.log("Rodando");
+});
